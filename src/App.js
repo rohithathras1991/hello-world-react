9b542a5648eacc6684b7edcd62b1db860721cb27
rohithathras1991 <99969494+rohithathras1991@users.noreply.github.com>
2022-03-08 22:01:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Gaurav Sharma - 14
+          Rohit Singh - 1
         </p>
         <a
           className="App-link"
